de0bcc1aa77daa2edf2ca42e8beb3b45d3e6ef63
syedeen <160634973+syedeen@users.noreply.github.com>
2025-03-09 18:59:23 +0530

    added event delegation && added close button

--- script.js
+++ script.js
@@ -18,19 +18,33 @@ let newYdir;
 
 let btn=document.querySelector(".create");
 btn.addEventListener("click",()=>{
+   
+
+    
+    //   card 
     let  div = document.createElement("div");
    div.classList.add("f");
    document.body.appendChild(div);
+  
+   
 
+   //    close btn 
+   let  close = document.createElement("div");
+   div.appendChild(close);
+   close.classList.add("close","fa-solid","fa-xmark");
+  
+})
 
-   div.addEventListener("mousedown", function(e){
-
-    
+   
+        // mousedown 
+   document.body.addEventListener("mousedown", function(e){
+          if(e.target.classList.contains("f")){        
+            let div=e.target;
         initXdir= e.clientX;
         initYdir= e.clientY;
 
-
-function mouseMove(e){
+        //  mousemove
+ function mouseMove(e){
     newXdir=initXdir-e.clientX;
     newYdir=initYdir-e.clientY;
     
@@ -42,12 +56,22 @@ function mouseMove(e){
 
 }
 
+
+//    mouseup 
 function mouseUp(){
 document.removeEventListener("mousemove",mouseMove);
 }
 
 document.addEventListener("mouseup",mouseUp);
 document.addEventListener("mousemove",mouseMove);
+ }
 });
-})
 
+
+     
+   document.body.addEventListener("click",function(e){
+     if(e.target.classList.contains("close")){
+            e.target.parentElement.remove();
+     }
+   })
+    
